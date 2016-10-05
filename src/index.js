8748c0279828fe5c8d2f54437be0eff5856270df
Horváth Győző <gyozke@inf.elte.hu>
2016-10-05 11:53:42 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+import 'bootswatch/cosmo/bootstrap.min.css'
+// import 'jquery/dist/jquery.min.js'
+// import 'bootstrap/dist/js/bootstrap.min.js'
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
